2c039e0f2b745cc143aefcdc91ad5e7c3d751ca5
Artemijs Zarovs <artemijs.zarovs@sap.com>
2023-06-03 13:03:27 +0300

    Basic openai setup

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+
+const openai = require('openai')
 const express = require('express')
 const app = express()
 const port = 3000
+
+const configuration = new openai.Configuration({
+    organization: "YOUR_ORG_ID",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new openai.OpenAIApi (configuration);
+const response = await openai.listEngines();
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
