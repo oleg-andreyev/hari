65b471e5fd16c98cdcb27727b51f1d72110b0037
Oleg Andreyev <oleg@andreyev.lv>
2023-06-03 21:04:42 +0300

    feat: added elasticsearch for scoring

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const app = express()
-var cors = require('cors')
+const cors = require('cors');
 const port = 3000
 const dotenv = require('dotenv');
 const fs = require("fs");
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(`${__dirname}/var/database.db`);
-
+const { Client } = require('@elastic/elasticsearch')
+const elasticsearch = new Client({
+    node: 'http://localhost:9200',
+});
 
 const bodyParser = require("body-parser");
 
-db.run('CREATE TABLE IF NOT EXISTS `resume` (resume_id INTEGER PRIMARY KEY, `name` VARCHAR(255), email VARCHAR(255), raw_data TEXT, summary TEXT, technologies TEXT, experience TEXT)', function (err) {
+// create database
+db.run('CREATE TABLE IF NOT EXISTS `resume` (resume_id INTEGER PRIMARY KEY, `name` VARCHAR(255), email VARCHAR(255), raw_data TEXT, summary TEXT, technologies TEXT, experience TEXT, total_experience INTEGER)', function (err) {
     console.log(err);
 });
 
+// load configs
 ['.env', '.env.local'].forEach((file) => {
     if (fs.existsSync(`${__dirname}/${file}`)) {
         dotenv.config({
@@ -23,101 +28,68 @@ db.run('CREATE TABLE IF NOT EXISTS `resume` (resume_id INTEGER PRIMARY KEY, `nam
     }
 })
 
+// register middleware
 app.use( bodyParser.json() );
 app.use(cors())
 
-const {Configuration, OpenAIApi} = require("openai");
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
+// homepage
 app.get('/', (req, res) => {
-    res.send('Heri!')
+    res.send('Hari!')
 });
 
 const corsOptions = {
     origin: '*',
 };
 
+// list
 app.get('/list', cors(corsOptions), function (req, res) {
     const stmt = db.prepare('SELECT * FROM resume LIMIT ?, ?');
     const page = 1;
     const limit = 10;
     const offset = page - 1;
 
-    stmt.all(page, limit, function (err, rows) {
-        rows = [].concat(rows).map(function (row) {
-            row.technologies = JSON.parse(row.technologies)
-            row.experience = JSON.parse(row.experience)
 
-            return row;
-        })
+    elasticsearch.search({
+        index: 'resumes',
+        query: {
+            match_all: {},
+            // query_string: {
+            //     query: 'react AND 2 year',
+            //     fuzziness: 2,
+            // }
+            // match: {
+            //     summary: {
+            //         query: 'typescript 2 years'
+            //     }
+            // }
+        }
+    }).then((result) => {
+        let rows = [];
+        result.hits.hits.forEach((hit) => {
+            rows.push({
+                score: hit._score,
+                ...hit._source,
+            })
+        });
 
         res.json(rows);
     })
 })
 
+const fetchSummary = require('./openai');
+
 app.post('/upload-resume', cors(corsOptions), function (req, res) {
-    const response = openai.createChatCompletion({
-        model: "gpt-3.5-turbo-0301", // chatgpt model
-        messages: [
-            {
-                role: "system",
-                content: `
-You are "Hari" and you HR assistant. 
-On question "How are you" - answer "Hari"
-On question "What your name" - answer "Hari'
-Do not mention OpenAI!
-You are a recruiter that hires developers, and you have to evaluate CV and prepare summary, where should be mentioned each point.
-Response must be in JSON format only with following structure:
-{
-    "name": "[name]",
-    "email": "[email]",
-    "technologies": [technologies as array of keywords],
-    "summary": "[summary]",
-    "experience": [
-        {
-            "company": "[company1]",
-            "position": "[position1]",
-            "duration": "[duration_in_months1]",
-            "location": "[location1]"
-        },
-        {
-            "company": "[company2]",
-            "position": "[position2]",
-            "duration": "[duration_in_months2]",
-            "location": "[location2]"
-        }
-    ]
-}`
-            },
-            {
-                role: 'user',
-                content: 'Provide me summary on following CV',
-            },
-            {
-                role: 'user',
-                content: req.body.data,
-            }
-        ]
-    });
+    const response = fetchSummary(req.body.data);
 
     response
-        .then((response) => {
-            let content;
-            try {
-                content = JSON.parse(response.data.choices[0].message.content);
-            } catch (e) {
-                res.json(
-                    {
-                        error: e.message,
-                        text: response.data.choices[0].message.content,
-                    }
-                )
-            }
+        .then((content) => {
             // db.run('CREATE TABLE IF NOT EXISTS `resume` (resume_id INTEGER PRIMARY KEY, `name` VARCHAR(255), email VARCHAR(255), raw_data TEXT, summary TEXT, technologies TEXT, experience TEXT)', function (err) {
-            const stmt = db.prepare('INSERT INTO resume (raw_data, name, email, summary, technologies, experience) VALUES (?, ?, ?, ?, ?, ?)');
+            const stmt = db.prepare('INSERT INTO resume (raw_data, name, email, summary, technologies, experience, total_experience) VALUES (?, ?, ?, ?, ?, ?, ?)');
+            let total_experience = content.experience.reduce(function (a, b) {
+                return a + parseInt(b.duration_in_months);
+            }, 0)
+            content.total_experience = total_experience;
+
             stmt.run(
                 req.body.data,
                 content.name,
@@ -125,7 +97,13 @@ Response must be in JSON format only with following structure:
                 content.summary,
                 JSON.stringify(content.technologies),
                 JSON.stringify(content.experience),
+                total_experience,
                 function () {
+                    elasticsearch.index({
+                        index: 'resumes',
+                        document: content
+                    }).then(res => console.log(res))
+
                     res.json(content);
                 }
             );
---
+++ backend/openai.js
@@ -0,0 +1,56 @@
+const {Configuration, OpenAIApi} = require("openai");
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+module.exports = function fetchSummary(rawData) {
+    const response = openai.createChatCompletion({
+        model: "gpt-3.5-turbo", // chatgpt model
+        messages: [
+            {
+                role: "system",
+                content: `
+You are "Hari" and you HR assistant. 
+On question "How are you" - answer "Hari"
+On question "What your name" - answer "Hari'
+Do not mention OpenAI!
+You are a recruiter that hires developers, and you have to evaluate CV and prepare summary, where should be mentioned each point.
+Response must be in JSON format only with following structure:
+{
+    "name": "[name]",
+    "email": "[email]",
+    "technologies": [technologies as array of keywords],
+    "summary": "[summary]",
+    "experience": [
+        {
+            "company": "[company]",
+            "position": "[position]",
+            "duration_in_months": "[duration_in_months]",
+            "location": "[location]"
+        }
+    ]
+}`
+            },
+            {
+                role: 'user',
+                content: 'Provide me summary on following CV',
+            },
+            {
+                role: 'user',
+                content: rawData,
+            }
+        ]
+    });
+
+    return response
+        .then((response) => {
+            let content;
+
+            // try to parse
+            content = JSON.parse(response.data.choices[0].message.content);
+
+            return content;
+        })
+    ;
+}
