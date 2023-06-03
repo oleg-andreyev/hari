40d23d94d8ba72a1bcf47b5fa3fe10e82a92ab61
Oleg Andreyev <oleg@andreyev.lv>
2023-06-03 21:38:01 +0300

    added reset script

---
+++ backend/reset.js
@@ -0,0 +1,13 @@
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({
+    node: 'http://localhost:9200',
+});
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(`${__dirname}/var/database.db`);
+
+client.indices.delete({
+    index: 'resumes'
+});
+
+db.run('DROP TABLE resumes');
