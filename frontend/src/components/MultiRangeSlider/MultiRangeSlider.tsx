e67fd06d4d832e22b5bb7415df422288b40ee8ee
emplv <emils@plavenieks.lv>
2023-06-04 18:33:22 +0300

    fix casing for include

--- frontend/src/components/MultiRangeSlider/MultiRangeSlider.tsx
+++ frontend/src/components/MultiRangeSlider/MultiRangeSlider.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState, useRef } from "react";
 import PropTypes from "prop-types";
-import "./multiRangeSlider.css";
+import "./MultiRangeSlider.css";
 
 const MultiRangeSlider: React.FC<{
   min: number;
